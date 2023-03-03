e93f642edab43e05cbbc4eca59ae1fbd968689f7
WilliamOnyejiaka <williamonyejiaka08062528003@gmail.com>
2023-03-03 09:22:07 +0100

    Updated the token attributes class

--- lib/TokenAttributes.php
+++ lib/TokenAttributes.php
@@ -7,15 +7,26 @@ class TokenAttributes
 
   public $access_token_exp_time;
   public $refresh_token_exp_time;
-  private $active_user;
+  private $data;
   private $access_token_aud;
 
-  public function __construct($active_user, $access_token_aud = "users", $exp_time = array())
+  public function __construct($data, $access_token_aud = "users", $exp_time = array())
   {
     $this->access_token_exp_time = isset($exp_time['access_token_exp_time']) ? $exp_time['access_token_exp_time'] : 3600;
     $this->refresh_token_exp_time = isset($exp_time['refresh_token_exp_time']) ? $exp_time['refresh_token_exp_time'] : 2592000;
     $this->access_token_aud = $access_token_aud;
-    $this->active_user = $active_user;
+    $this->data = $data;
+  }
+
+  public function serialize()
+  {
+    $needed_values = $this->data['needed_values'];
+    $user_data = $this->data['data'];
+    $serialized_data = array();
+    foreach ($needed_values as $key) {
+      $serialized_data[$key] = $user_data[$key];
+    }
+    return $serialized_data;
   }
 
   public function access_token_payload()
@@ -24,9 +35,7 @@ class TokenAttributes
     $nbf = $iat;
     $exp = $iat + $this->access_token_exp_time;
     $aud = $this->access_token_aud;
-    $user_data = array(
-      'id' => $this->active_user['id'],
-    );
+    $user_data = $this->serialize();
 
     $payload = array(
       'iat' => $iat,
@@ -44,9 +53,7 @@ class TokenAttributes
     $nbf = $iat;
     $exp = $iat + $this->refresh_token_exp_time;
     $aud = "access_token";
-    $user_data = array(
-      'id' => $this->active_user['id'],
-    );
+    $user_data = $this->serialize();
 
     $payload = array(
       'iat' => $iat,
