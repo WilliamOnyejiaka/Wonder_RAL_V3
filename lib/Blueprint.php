4347055826f59b2f534088dd57d43f424e5572af
William Onyejiaka <williamonyejiaka08062528003@gmail.com>
2023-09-05 20:55:33 +0100

    added the Blueprint class

---
+++ lib/Blueprint.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Lib;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+use Lib\Response;
+use Lib\Validator;
+use Lib\Request;
+
+ini_set("display_errors", 1);
+
+class Blueprint
+{
+
+}
